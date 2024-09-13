871330d1fd66fc00fb46f482fa79bfa561090633
sangamhoni <87515320+subsangam@users.noreply.github.com>
2024-09-13 03:27:38 -0500

    Create Display Label

--- calc.py
+++ calc.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 
+SMALL_FONT_STYLE=("Arial", 16)
+LARGE_FONT_STYLE=("Arial", 40, "bold")
 LIGHT_GRAY="#f5f5f5"
+LABEL_COLOR="#25265E"
 
 class Calculator:
     def __init__(self):
@@ -8,12 +11,23 @@ class Calculator:
         self.window.geometry("375x667") 
         self.window.resizable(0, 0)
         self.window.title("GUI Calculator")
-    
+
+        self.total_expression = "0"
+        self.current_expression = "0"
+
         self.display_frame=self.create_display_frame()
         self.buttons_frame=self.create_buttons_frame()
         
-    def run(self):
-        self.window.mainloop()
+        self.total_label, self.label=self.create_display_labels()
+
+    def create_display_labels(self):
+        total_label=tk.Label(self.display_frame, text=self.total_expression, anchor=tk.E, bg=LIGHT_GRAY, fg=LABEL_COLOR, padx=24, font=SMALL_FONT_STYLE) # here, E in anchor means East
+        total_label.pack(expand=True, fill="both")
+        
+        label=tk.Label(self.display_frame, text=self.current_expression, anchor=tk.E, bg=LIGHT_GRAY, fg=LABEL_COLOR, padx=24, font=LARGE_FONT_STYLE) # here, E in anchor means East
+        label.pack(expand=True, fill="both")
+
+        return total_label, label
 
     def create_display_frame(self):
         frame=tk.Frame(self.window, height=221, bg=LIGHT_GRAY)
@@ -22,10 +36,13 @@ class Calculator:
     
     def create_buttons_frame(self):
         frame=tk.Frame(self.window)
-        frame.pack(expand=True, fill="both")
+        frame.pack(expand=True, fill="both") #both meaning it should expand both horizontally and vertically
         return frame
 
-if __name__=="__main__":
+    def run(self):
+        self.window.mainloop()
 
+
+if __name__=="__main__":
     calc=Calculator()
     calc.run()
